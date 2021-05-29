3369b9f16bfe14594e696b1448f5af3a9211290d
Fumieizu <71212206+Fumieizu@users.noreply.github.com>
2021-05-29 21:46:46 +0500

    fix

--- src/presenter/content-list.js
+++ src/presenter/content-list.js
@@ -106,7 +106,9 @@ export default class FilmBoard {
           .values(this._filmPresenters)
           .forEach((presenter) => {
             if (update.id in presenter) {
-              presenter[update.id].setViewState(State.SAVING, comments);
+              if (presenter[update.id].hasOpenPopup()) {
+                presenter[update.id].setViewState(State.SAVING, comments);
+              }
             }
           });
         this._api.addComment(update, comments).then((response) => {
